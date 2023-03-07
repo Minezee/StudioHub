a3df8b03d195daa2572fa58b2a68fb68c11d0242
Nauval Fahreza A <nauvalfahreza03@gmail.com>
2023-03-08 00:42:04 +0700

    add component and container for studio information

--- src/pages/StudioInformation.jsx
+++ src/pages/StudioInformation.jsx
@@ -3,6 +3,9 @@ import PageWrapper from "@/components/layout/PageWrapper"
 import Layout from "@/components/layout/Layout"
 import Detail from "@/containers/StudioInformation/Detail"
 import Facility from "@/containers/StudioInformation/Facility"
+import OrderContainer from "@/containers/StudioInformation/OrderContainer"
+import Ratings from "@/components/global/Ratings"
+import Comment from "@/containers/StudioInformation/Comment"
 
 const StudioInformation = () => {
     return (
@@ -11,6 +14,9 @@ const StudioInformation = () => {
             <ResultHeader />
             <Detail />
             <Facility />
+            <OrderContainer />
+            <Ratings />
+            <Comment />
         </PageWrapper>
     </Layout>
     )
