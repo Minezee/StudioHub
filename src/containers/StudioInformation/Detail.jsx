db6ad55a969e77ac57ffafa15d9f799064411495
Nauval Fahreza A <nauvalfahreza03@gmail.com>
2023-03-08 00:40:56 +0700

    implement design system for font size

--- src/containers/StudioInformation/Detail.jsx
+++ src/containers/StudioInformation/Detail.jsx
@@ -16,15 +16,15 @@ const Detail = () => {
         <div className='bg-card-bg px-14 py-6 rounded-[20px] text-white'>
             <div className="flex items-start justify-between">
                 <div>
-                    <h2 className="text-orange-500 text-5xl font-semibold">Corner Music Studio</h2>
+                    <h2 className="text-orange-500 text-h2 font-semibold">Corner Music Studio</h2>
                     <p className="text-2xl font-semibold">Lowokwaru, Malang</p>
                 </div>
                 <DisplayStar rating={4} lg />
             </div>
             <DisplayImage />
             <div className="mt-20 flex justify-between items-center font-bold">
-                <p className="text-[2.5rem] text-orange-500">Rp 50.000</p>
-                <button onClick={() => scrollToSection(orderSection)} className="bg-orange-500 px-[4.25rem] py-4 rounded-lg text-2xl">
+                <p className="text-h3 text-orange-500">Rp 50.000</p>
+                <button onClick={() => scrollToSection(orderSection)} className="bg-orange-500 px-[4.25rem] py-4 rounded-lg text-h5">
                     Order Now
                 </button>
             </div>
