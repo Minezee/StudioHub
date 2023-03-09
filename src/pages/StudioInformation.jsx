3722d1b7f16b087405f0ce8b6a6bdfb2866b350c
Nauval Fahreza A <nauvalfahreza03@gmail.com>
2023-03-09 19:58:10 +0700

    add ref

--- src/pages/StudioInformation.jsx
+++ src/pages/StudioInformation.jsx
@@ -1,24 +1,25 @@
-import { ResultHeader } from "@/components/global"
+import { ResultHeader, CommentSection, Ratings } from "@/components/global"
 import PageWrapper from "@/components/layout/PageWrapper"
 import Layout from "@/components/layout/Layout"
-import Detail from "@/containers/StudioInformation/Detail"
+import DetailStudio from "@/containers/StudioInformation/DetailStudio"
 import Facility from "@/containers/StudioInformation/Facility"
 import OrderContainer from "@/containers/StudioInformation/OrderContainer"
-import Ratings from "@/components/global/Ratings"
-import Comment from "@/containers/StudioInformation/Comment"
+import { useRef } from "react"
 
 const StudioInformation = () => {
+    const orderSection = useRef(null);
+
     return (
         <Layout>
-        <PageWrapper>
-            <ResultHeader />
-            <Detail />
-            <Facility />
-            <OrderContainer />
-            <Ratings />
-            <Comment />
-        </PageWrapper>
-    </Layout>
+            <PageWrapper>
+                <ResultHeader />
+                <DetailStudio orderSection={orderSection}/>
+                <Facility />
+                <OrderContainer orderSection={orderSection}/>
+                <Ratings />
+                <CommentSection />
+            </PageWrapper>
+        </Layout>
     )
 }
 
