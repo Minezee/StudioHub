e86944fd1ff413047ce6f104d2053c0e0f174f8b
Nauval Fahreza A <nauvalfahreza03@gmail.com>
2023-03-09 19:57:38 +0700

    change api base url

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://103.189.235.157:10002/api/v1";
+const API_URL = "https://daffarel.aenzt.tech/api/v1";
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
