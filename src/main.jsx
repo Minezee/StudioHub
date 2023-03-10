76d09e27d1d8dfb62e8704d27bc8d2fdc40b692c
Nauval Fahreza A <nauvalfahreza03@gmail.com>
2023-03-10 20:32:21 +0700

    add OrderProvider

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,20 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { FilterProvider } from './context/FilterContext'
+import { OrderProvider } from './context/OrderContext'
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <FilterProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </FilterProvider>
+      <OrderProvider>
+        <FilterProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </FilterProvider>
+      </OrderProvider>
     </QueryClientProvider>
   </React.StrictMode>,
 )
