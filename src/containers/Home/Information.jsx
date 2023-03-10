3b5addab14a136d72cfb419cc9e8e9a655cccf5f
Nauval Fahreza A <nauvalfahreza03@gmail.com>
2023-03-10 20:36:38 +0700

    add responsive style

--- src/containers/Home/Information.jsx
+++ src/containers/Home/Information.jsx
@@ -3,7 +3,7 @@ import { lomba } from '@/assets'
 
 const Information = () => {
     return (
-        <section className='w-full bg-yellow-400 mt-20 grid grid-cols-3 gap-4 md:gap-10 place-items-center py-5 md:py-12 lg:py-9 rounded-lg lg:rounded-[20px] px-4 md:px-10'>
+        <section className='w-full bg-transparent sm:bg-yellow-400 mt-10 sm:mt-20 grid grid-cols-3 gap-4 md:gap-10 place-items-center p-0 sm:py-5 md:py-12 lg:py-9 rounded-lg lg:rounded-[20px] px-0 sm:px-4 md:px-10'>
             {[1, 2, 3].map(data => (
                 <div key={data + "event"} className='h-auto'>
                     <img src={lomba} alt="" className='w-full h-full' />
