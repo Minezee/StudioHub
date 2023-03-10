af31f190a80c4cdab54aa4350bc918e04568fe5f
Nauval Fahreza A <nauvalfahreza03@gmail.com>
2023-03-10 20:34:19 +0700

    make more data dummy

--- src/containers/Home/BigDeals.jsx
+++ src/containers/Home/BigDeals.jsx
@@ -1,7 +1,7 @@
 import StudioContainer from "./StudioContainer"
 
 const BigDeals = () => {
-    return <StudioContainer title={"BIG DEALS"} paragraph={"This week's trending studio"} studio={[1, 2, 3, 4, 5]} />
+    return <StudioContainer title={"BIG DEALS"} paragraph={"This week's trending studio"} studio={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]} />
 }
 
 export default BigDeals
--- src/containers/Home/Trending.jsx
+++ src/containers/Home/Trending.jsx
@@ -1,7 +1,7 @@
 import StudioContainer from './StudioContainer';
 
 const Trending = () => {
-    return <StudioContainer title={"TRENDING STUDIOS"} paragraph={"This week's trending studio"} studio={[1, 2, 3, 4, 5]}/>
+    return <StudioContainer title={"TRENDING STUDIOS"} paragraph={"This week's trending studio"} studio={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]}/>
 }
 
 export default Trending
