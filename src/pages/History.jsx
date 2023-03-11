777b8b81aead3d11ff16ee9a0b8141c822ce98a8
Nauval Fahreza A <nauvalfahreza03@gmail.com>
2023-03-11 11:07:53 +0700

    create history page (not done yet)

---
+++ src/pages/History.jsx
@@ -0,0 +1,12 @@
+import PageWrapper from "@/components/layout/PageWrapper"
+import BackNav from "@/components/common/BackNav"
+
+const History = () => {
+    return (
+        <PageWrapper>
+            <BackNav />
+        </PageWrapper>
+    )
+}
+
+export default History
