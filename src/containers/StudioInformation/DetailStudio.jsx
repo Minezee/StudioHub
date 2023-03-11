0bd3e4df4a8b1808cb38ec3ece6282569cf4f388
Nauval Fahreza A <nauvalfahreza03@gmail.com>
2023-03-11 11:13:18 +0700

    change code abit

--- src/containers/StudioInformation/DetailStudio.jsx
+++ src/containers/StudioInformation/DetailStudio.jsx
@@ -1,8 +1,9 @@
 import { DisplayImage, DetailHeader } from "@/components/global"
 import styles from "@/styles/Styles.module.css"
 
-const DetailStudio = ({orderSection}) => {
-    const scrollToSection = (ref) => {
+const DetailStudio = ({ orderSection }) => {
+    
+    function scrollToSection(ref) {
         const sectionElement = ref.current;
         if (sectionElement) {
             const scrollPosition = sectionElement.offsetTop - window.innerHeight / 2.2 + sectionElement.offsetHeight / 2;
@@ -12,7 +13,7 @@ const DetailStudio = ({orderSection}) => {
 
     return (
         <div className='bg-card-bg px-4 sm:px-14 py-6 rounded-[20px] text-white'>
-            <DetailHeader name="Corner Music Studio" location="Lowokwaru, Malang" rate={4}/>
+            <DetailHeader name="Corner Music Studio" location="Lowokwaru, Malang" rate={4} />
             <DisplayImage />
             <div className="mt-10 sm:mt-20 flex justify-between items-center font-bold">
                 <p className="text-h4 sm:text-h3 text-orange-500">Rp 50.000</p>
