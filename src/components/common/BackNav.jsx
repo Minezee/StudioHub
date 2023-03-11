3092d33df6c6fe8f9305763c3f9fbe2791083664
Nauval Fahreza A <nauvalfahreza03@gmail.com>
2023-03-11 11:06:40 +0700

    create back nav for some page

---
+++ src/components/common/BackNav.jsx
@@ -0,0 +1,20 @@
+import { IoIosArrowBack } from "react-icons/io"
+import { NavLink } from "react-router-dom"
+import { logo } from "@/assets"
+
+const BackNav = () => {
+    return (
+        <div className="flex justify-between items-center mt-14 relative mb-7">
+            <NavLink to={-1} className="flex text-h5 font-bold gap-2 items-center text-white">
+                <IoIosArrowBack />
+                BACK
+            </NavLink>
+            <NavLink to={'/'} className="flex text-h4 font-bold gap-2 items-center text-yellow-500">
+                <img src={logo} alt="logo" className="w-7" />
+                STUDIO HUB
+            </NavLink>
+        </div>
+    )
+}
+
+export default BackNav
