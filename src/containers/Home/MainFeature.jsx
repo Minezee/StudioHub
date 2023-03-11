dd22cc59a7e87e8f23a50d317768577cdd00ff9c
Nauval Fahreza A <nauvalfahreza03@gmail.com>
2023-03-11 12:57:22 +0700

    change icon color

--- src/containers/Home/MainFeature.jsx
+++ src/containers/Home/MainFeature.jsx
@@ -10,7 +10,7 @@ const MainFeature = ({feature}) => {
             <div className="mt-10 flex items-center justify-center gap-10 sm:gap-[6.25rem] text-white text-3xl sm:text-[2.5rem] font-black">
                 {feature.map(menu => (
                 <div key={menu.name} className="bg-yellow-400 w-full sm:w-48 aspect-square sm:h-[12.5rem] flex flex-col items-center justify-center rounded-2xl text-dark">
-                    <menu.icon className="h-[50%] sm:h-[5.5rem] w-auto text-white" />
+                    <menu.icon className="h-[50%] sm:h-[5.5rem] w-auto" />
                     {menu.name}
                 </div>
                 ))}
