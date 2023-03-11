ab91e1a6f7c6c1ee85b99a89da242718cfd08d1a
Nauval Fahreza A <nauvalfahreza03@gmail.com>
2023-03-11 13:40:25 +0700

    change height of slider

--- src/containers/Home/StudioContainer.jsx
+++ src/containers/Home/StudioContainer.jsx
@@ -15,7 +15,7 @@ const StudioContainer = ({ title, paragraph, studio }) => {
             slidesPerGroup={5}
             initialSlide={0}
             loop
-            className='mySwiper hidden lg:block mt-9'>
+            className='mySwiper hidden md:block mt-9'>
                 {studio.map(data => (
                     <SwiperSlide key={data} className='text-2xl flex flex-col gap-3 cursor-pointer'>
                         <div className='h-[15.25rem] rounded-xl'>
@@ -35,10 +35,10 @@ const StudioContainer = ({ title, paragraph, studio }) => {
             slidesPerGroup={3}
             initialSlide={0}
             loop
-            className='mySwiper block lg:hidden mt-9'>
+            className='mySwiper block md:hidden mt-9'>
                 {studio.map(data => (
                     <SwiperSlide key={data} className='text-2xl flex flex-col gap-3 cursor-pointer'>
-                        <div className='h-[15.25rem] rounded-xl'>
+                        <div className='h-[10.25rem] sm:h-[14rem] rounded-xl'>
                             <img src={display_studio} alt="" className='h-full w-full rounded-xl' />
                         </div>
                         <h3>Corner Music Studio {data}</h3>
